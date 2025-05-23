b8ac45dda594a4177e0ded1fbecbd72dd4b322a1
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-05-23 18:23:09 -0300

    Autenticação de admin e aluno utilizando breeze

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Aluno;
+
+class AlunoController extends Controller
+{
+    public function dashboard()
+    {
+        return view('aluno.dashboard');
+    }
+
+    public function index()
+    {
+        $alunos = Aluno::all();
+
+        return view('alunos.index')->with('alunos', $alunos);
+    }
+
+    public function create()
+    {
+        return view('alunos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nome' => 'required|string|max:255',
+            'cpf' => 'required|max:14',
+            'email' => 'required|max:255|email',
+            'telefone' => 'max:15',
+            'senha' => 'required|string|min:8',
+            'curso_id' => 'required|exists:cursos, id',
+            'turma_id' => 'required|exists:turmas, id',
+        ]);
+
+        Aluno::create([
+            'nome' => $validated['nome'],
+            'cpf' => $validated['cpf'],
+            'email' => $validated['email'],
+            'telefone' => $validated['telefone'],
+            'senha' => $validated['senha'],
+            'curso_id' => $validated['curso_id'],
+            'turma_id' => $validated['turma_id'],
+        ]);
+
+        return redirect()->route('alunos.index')->with('success', 'Aluno criado com sucesso!');
+    }
+
+    public function show(string $id)
+    {
+        $aluno = Aluno::findOrFail($id);
+
+        return view('alunos.show')->with('aluno', $aluno);
+    }
+
+    public function edit(string $id)
+    {
+        $aluno = Aluno::findOrFail($id);
+
+        return view('alunos.edit')->with('aluno', $aluno);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $aluno = Aluno::findOrFail($id);
+
+        $validated = $request->validate([
+            'nome' => 'required|string|max:255',
+            'cpf' => 'required|max:14',
+            'email' => 'required|max:255|email',
+            'telefone' => 'max:15',
+            'senha' => 'required|string|min:8',
+        ]);
+
+
+        $aluno->nome = $validated['nome'];
+        $aluno->cpf = $validated['cpf'];
+        $aluno->email = $validated['email'];
+        $aluno->telefone = $validated['telefone'];
+
+        $aluno->save();
+
+        return redirect()->route('alunos.index')->with('success', 'Aluno atualizado com sucesso!');
+    }
+
+    public function destroy(string $id)
+    {
+        $aluno = Aluno::findOrFail($id);
+
+        $aluno->delete();
+
+        return redirect()->route('alunos.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AlunoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\Role;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', Role::class . ':admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])
+        ->name('admin.dashboard');
+});
+Route::middleware(['auth', Role::class . ':aluno'])->group(function () {
+    Route::get('aluno/dashboard', [AlunoController::class, 'dashboard'])
+    ->name('aluno.dashboard');
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
